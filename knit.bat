cfe1e50ad543c256e0048b8f320adda4c6133e45
Yoav Ram <yoavram+github@gmail.com>
2013-02-28 12:39:43 +0200

    knit html and pdf

--- knit.bat
+++ knit.bat
@@ -1,2 +1,3 @@
-..\nbconvert\nbconvert.py  -f latex recitation1.ipynb
+python d:\workspace\nbconvert\nbconvert.py  -f html recitation1.ipynb
+python d:\workspace\nbconvert\nbconvert.py  -f latex recitation1.ipynb
 pandoc recitation1.tex -o recitation1.pdf -s -S
