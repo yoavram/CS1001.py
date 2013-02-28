105cb16eece8c4b51775424809afab36553ecd5f
Yoav Ram <yoavram+github@gmail.com>
2013-02-28 12:43:32 +0200

    no need for html version, but full path to nbconvert is nice

--- knit.bat
+++ knit.bat
@@ -1,3 +1,2 @@
-python d:\workspace\nbconvert\nbconvert.py  -f html recitation1.ipynb
 python d:\workspace\nbconvert\nbconvert.py  -f latex recitation1.ipynb
 pandoc recitation1.tex -o recitation1.pdf -s -S
